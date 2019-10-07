503885c9b0acfc1cbe7221096ddec4dc4bfbc10c
Nguyen Trong Hieu <tronghieu8531@gmail.com>
2019-10-07 11:08:32 +0700

    + Add android project and document

---
+++ android_app/app/src/main/java/com/team4of5/foodspotting/MainActivity.java
@@ -0,0 +1,14 @@
+package com.team4of5.foodspotting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
