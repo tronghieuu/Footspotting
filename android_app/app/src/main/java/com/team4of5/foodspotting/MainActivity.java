5f9910de2e7ea52e18745230571ae8cb00679398
bluebird <nttruclol@gmail.com>
2019-10-08 10:42:26 +0700

    modify MainActivity.java ,when playing app on HomeFragment

--- android_app/app/src/main/java/com/team4of5/foodspotting/MainActivity.java
+++ android_app/app/src/main/java/com/team4of5/foodspotting/MainActivity.java
@@ -1,8 +1,13 @@
 package com.team4of5.foodspotting;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+                    switch (menuItem.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_list:
+                            selectedFragment = new ListFragment();
+                            break;
+                        case R.id.nav_notifications:
+                            selectedFragment = new NotificationFragment();
+                            break;
+                        case R.id.nav_personal:
+                            selectedFragment = new PersonFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    return true;
+                }
+            };
 }
