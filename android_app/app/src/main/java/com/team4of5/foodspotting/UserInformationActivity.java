3f0327d5aa7cd912918a88d0e5e16c2f3707fc5e
bluebird <nttruclol@gmail.com>
2019-10-29 16:55:38 +0700

    add UserInformation activity

---
+++ android_app/app/src/main/java/com/team4of5/foodspotting/UserInformationActivity.java
@@ -0,0 +1,14 @@
+package com.team4of5.foodspotting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_information);
+    }
+}
