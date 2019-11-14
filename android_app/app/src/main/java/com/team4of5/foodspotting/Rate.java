3c1a2392a0797732f8114b5d6082f7c0e6771a68
noobv24 <56160863+noobv24@users.noreply.github.com>
2019-11-14 07:56:50 +0700

    Update Rate.java

--- android_app/app/src/main/java/com/team4of5/foodspotting/Rate.java
+++ android_app/app/src/main/java/com/team4of5/foodspotting/Rate.java
@@ -22,7 +22,6 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
