5c63ac5e16ff535614ef30f3920cb5e856335ac0
bluebird <nttruclol@gmail.com>
2019-11-19 11:36:42 +0700

    dang ky shipper

---
+++ android_app/app/src/main/java/com/team4of5/foodspotting/RegisterShipperActivity.java
@@ -0,0 +1,56 @@
+package com.team4of5.foodspotting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class RegisterShipperActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mBtnBack, mBtnCreate;
+    private Dialog dialog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_shipper);
+        getSupportActionBar().hide();
+        Window window = this.getWindow();
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        mBtnBack.setOnClickListener(this);
+        mBtnCreate.setOnClickListener(this);
+        dialog = new Dialog(this);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.item_loading);
+        dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialogInterface) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnBackShipperDangKy:
+                finish();
+                break;
+            case R.id.btnShipperApp:
+                Intent intent = new Intent(getApplicationContext(), Shipper.class);
+                dialog.dismiss();
+                startActivity(intent);
+                break;
+        }
+    }
+}
