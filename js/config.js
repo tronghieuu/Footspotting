ec1b450f0b59896d2af03f27913b826d9c689de4
12 Phong <phong7761234@gmail.com>
2019-11-21 06:57:28 +0700

    web admin

---
+++ js/config.js
@@ -0,0 +1,49 @@
+(function(){
+    const firebaseConfig = {
+    apiKey: "AIzaSyA6PG2O-YPFDQwBgVhXijnIIAs9SO3vtY4",
+    authDomain: "foodspotting-11369.firebaseapp.com",
+    databaseURL: "https://foodspotting-11369.firebaseio.com",
+    projectId: "foodspotting-11369",
+    storageBucket: "foodspotting-11369.appspot.com",
+    messagingSenderId: "863159936668",
+    appId: "1:863159936668:web:6b002c336e5ed6c4daef59",
+    measurementId: "G-6QJ312FVNV"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
+
+const txtEmail = document.getElementById('txtEmail');
+const txtPassword = document.getElementById('txtPassword');
+const btnLogin = document.getElementById('btnLogin');
+
+btnLogin.addEventListener('click', e =>{
+    const email = txtEmail.value;
+    const Password = txtPassword.value;
+    const auth = firebase.auth();
+    const promise = auth.signInWithEmailAndPassword(email, Password);
+    console.log("Here");
+    promise.catch(e => console.log(e.message))
+});
+
+
+
+// btnLogin.addEventListener('click',e=>{
+//     const email = txtEmail;
+//     const Password = t   xtPassword;
+//     const auth = firebase.auth();
+//     const promise = auth.signInWithEmailAndPassword(email, Password);
+
+//     promise.catch(e => console.log(e.message))
+// })
+
+firebase.auth().onAuthStateChanged(firebaseUser =>{
+    if(firebaseUser){
+        consolele.log(firebaseUser);
+        // window.location="index.html";
+    } else{
+        console.log('not logged in');
+        // window.location.reload(); 
+    }
+});
+}());
